ed05b6f241388fa17dcf714bc0b45c20b34e69fb
Tonsakhone <51913353+Tonsakhone@users.noreply.github.com>
2019-06-24 05:14:52 -0700

    Update login.php

--- user/login.php
+++ user/login.php
@@ -2,6 +2,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title></title>
 <!--css-->
 <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all"/>
@@ -9,7 +10,7 @@
 <link href="css/font-awesome.css" rel="stylesheet">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
 <!--css-->
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <body>
 	
